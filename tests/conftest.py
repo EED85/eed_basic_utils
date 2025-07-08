06c5f3b810b131f79344d59070db1f91c3bed26c
EED85 <eric.brahmann@eed-solutions.de>
2025-07-08 22:15:38 +0200

    klappt noch nicht

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import os
+from collections.abc import Iterator
+from pathlib import Path
+
+
+import pytest
+
+
+
+
+
+
+@pytest.fixture(scope="session")
+def cfg_test(db_setup) -> Iterator[dict]:
+    home_dir = os.path.expanduser("~")
+    cfg_test = {
+        "home_dir": home_dir,
+    }
+    yield cfg_test
+
+
+@pytest.fixture(scope="session")
+def prepare_files(cfg_test):
+    import tempfile
+
+    tempdir = Path(tempfile.gettempdir())
+    file_path = tempdir / "existing_file.txt"
+    file_path.touch()
+    cfg_test["tempdir"] = tempdir
+    yield cfg_test
+    file_path.unlink()
